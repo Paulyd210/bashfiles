87d22742294c9adbf05f13d9b440524f2ddad130
ramon_esparza1234 <rodriguez.esparza.ramon@gmail.com>
2018-03-08 10:03:18 -0600

    Added script that adds user

---
+++ WickedShellScriptsbook/adduser.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# adduser, Add a new user to the system, including building their home directory
+# copying in default data.
+
+pwfile="/etc/passwd"
+shadowfile="/etc/shadow"
+gfile="/etc/group"
+hdir="/home"
+
+if [ "$(id -un)" != root ]; then
+	echo "Error: MUST BE RUN AS ROOT" >&2
+	exit 1
+fi
+
+
+echo "Add new user account to $(hostname)"
+echo -n "login:	"
+read login
+
+# The next line sets the highest possible user ID value at 5000
+# but you should adjust this number to match the top end
+# of your user ID range
+uid="$(awk -F: '{ if(big<$3&&$3<5000)big=$3 } END { print big + 1 }'\
+	$pwfile)"
+
+
+homedir=$hdir/$login
+# We are giving each user their own group
+gid=$uid
+echo -n "full name: "
+read fullname
+echo -n "shell: "
+read shell
+
+echo "Setting up account $login for $fullname"
+echo ${login}:x:${uid}:${gid}:${fullname}:${homedir}:$shell >> $pwfile
+echo ${login}:*:11647:0:99999:7:::>>$shadowfile
+
+echo "${login}:x:${gid}:$login">>$gfile
+
+mkdir $homedir
+cp -R /etc/skel/.[a-zA-Z]* $homedir
+chmod 755 $homedir
+chown -R ${login}:${login} $homedir 
+
+#Setting an initial password
+exec passwd $login
